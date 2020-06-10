4c08c8827fae5f89a3c8e327630c8f96c74673f3
HandSomeBoyBIG <big@big>
2020-06-10 19:26:12 +0800

    写了一个实体类

---
+++ src/main/java/com/big/entity/Student.java
@@ -0,0 +1,21 @@
+package com.big.entity;
+
+import java.io.Serializable;
+
+public class Student implements Serializable{
+	private Integer sid;
+	private String sname;
+	public Integer getSid() {
+		return sid;
+	}
+	public void setSid(Integer sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	
+}
